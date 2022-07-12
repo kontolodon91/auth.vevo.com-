62b7d77ce989dda341ac131b452814889dee0b77
kontolodon91 <67394127+kontolodon91@users.noreply.github.com>
2022-07-12 15:35:20 -0700

    Add files via upload

---
+++ 0.php
@@ -0,0 +1,28 @@
+
+<!--Simple Page Defacement By l4ngroot Copyright ÃƒÂ¯Ã‚Â¿Ã‚Â½ 2020-->
+<!--Hargailah Karya Oranglain :)-->
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>wh00ps!</title>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<meta name="description" content="Hacked by ./G1L4N6_ST86">
+<meta name="keywords" content="Hacked , by ./G1L4N6_ST86">
+<meta http-equiv="cache-control" content="index,cache">
+<meta http-equiv="pragma" content="index,cache">
+<meta name="theme-color" content="black">
+<center>
+<table width=100% height=100%><td align=center>
+<body bgcolor="black">
+<img border="0" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5d208609-e803-4895-a7ad-e8f3d4692e52/dwrtjc-b7dacc68-1a7c-416b-9f9e-740013d4d5c0.jpg/v1/fill/w_233,h_350,q_70,strp/weysetvbubu_by_ahhaha_dwrtjc-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvNWQyMDg2MDktZTgwMy00ODk1LWE3YWQtZThmM2Q0NjkyZTUyXC9kd3J0amMtYjdkYWNjNjgtMWE3Yy00MTZiLTlmOWUtNzQwMDEzZDRkNWMwLmpwZyIsIndpZHRoIjoiPD02MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.r-t9Jg1GvXRoLTBqg3XYCuf-IKRWAO-qbNdoipReihw" width="440" height="250">
+<br>
+<i><font size="4" face="Inconsolata" color="white"># g0t pwn3d by ./G1L4N6_ST86 - AnonSec Team #
+<br>
+<br>
+<font size="3" color="red">" Vuln!! :D please update your system zuahaha xD "
+<br>
+<br>
+<font color="white" size="2">
+<br></font>
+<embed src="https://b.top4top.io/m_1665ak6j10.mp3" type="audio/mpeg"autostart="true" loop="true" height="0" width="0">
+<body>
+</html>
